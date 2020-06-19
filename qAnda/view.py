35b67e57cd696543e8f9b4b4313eb32bada360d6
isysc1 <46256542+isysc1@users.noreply.github.com>
2020-06-19 18:39:03 +0800

    Update view.py

--- qAnda/view.py
+++ qAnda/view.py
@@ -5,26 +5,25 @@ import json
 from django.shortcuts import render
 import requests
 
-list = [210]
+seaNum = [448962]
+
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {"count": seaNum[0]});
 
 
 def search(request):
-    list[0] = list[0] + 1
-    keyWord = request.POST.get("question")
-    url = 'https://api.deeract.com/l2s/api/questions?keyword=' + keyWord
-    print(url)
+    seaNum[0] = seaNum[0] + 1
+    key_word = request.POST.get("question")
+    url = 'https://api.deeract.com/l2s/api/questions?keyword=' + key_word
     result = requests.get(url)
     user_dict = json.loads(result.text)
-    answerId = ['title1', 'title2', 'title3', 'title4', 'title5', 'title6', 'title7']
+    answer_id = []
     answer = []
     for i in user_dict:
         answer.append(i['title'])
-    answerContext = dict(zip(answerId, answer))
-    answerContext['countService'] = list[0]
-    print(answerContext)
-    return render(request, "index.html", answerContext)
-
-
+    for j in range(len(answer)):
+        answer_id_name = "title" + str(j)
+        answer_id.append(answer_id_name)
+    answer_context = dict(zip(answer_id, answer))
+    return render(request, "result.html", {"answer_context": answer_context})
